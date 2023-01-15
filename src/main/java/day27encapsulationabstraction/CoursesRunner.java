57f26fafe49224a4d3ee62c65ebbefeeec90090e
Sefa Eyer <eyersefa@gmail.com>
2023-01-16 00:21:35 +0300

    14.01.2023  java

---
+++ src/main/java/day27encapsulationabstraction/CoursesRunner.java
@@ -0,0 +1,21 @@
+package day27encapsulationabstraction;
+
+public class CoursesRunner {
+
+    public static void main(String[] args) {
+
+       //abstract class larin icinde abstract method lar vardir.
+        // abstract method larin body si olmadigindan bu method lar yarim method sayilir.
+        // dolayisiyla abstract class lar eksigi olan kaliplar gibi dusunulebilir.
+        // Java eksik kaliplarin obje uretmesine musade etmez.
+        // bu yuzden abstract class lardan obje uretilemez.
+
+//INTERVIEW //abstract class lardan obje olusturamayiz.
+        //abstract class larin constructor i vardir ama bu constructor obje uretemez.
+
+        // Courses myCourse = new Courses();
+
+
+
+    }
+}
---
+++ src/main/java/day27encapsulationabstraction/Rooms.java
@@ -0,0 +1,10 @@
+package day27encapsulationabstraction;
+
+public  abstract class Rooms extends Courses {
+
+    //abstract class larin parent taki abstract method lari override etme zorunlulugu yoktur.
+    //mesela rooms class abstract class tir. rooms classi course abstract class inin child i
+    //  yaptigim ve override yapmadigim halde code sikayet etmiyor.
+
+
+}
